7f568ced3a29c90a279c1d219d69a1a2e2890d02
SymWhy <simone.whysel@gmail.com>
2026-04-27 23:37:40 -0400

    rename and rescope program

--- src/background.js
+++ src/background.js
@@ -1,7 +1,6 @@
-console.log("Attempting to run bg script...");
 
 // List of available fact-checking sources
-const factcheckers = [
+const searchengines = [
   { id: 'wikipedia', title: 'Wikipedia', url: 'https://en.wikipedia.org/wiki/Special:Search?search=', logo: 'res/logos/wikipedia-32.png', enabled: true },
   { id: 'snopes', title: 'Snopes', url: 'https://www.snopes.com/search/?q=', logo: 'res/logos/snopes-32.png', enabled: true },
   { id: 'politifact', title: 'Politifact', url: 'https://www.politifact.com/search/?q=', logo: 'res/logos/politifact-32.png', enabled: true },
@@ -11,52 +10,45 @@ const factcheckers = [
 // Add new option to the context menu
 browser.contextMenus.create(
   {
-    id: "contextify",
-    title: "Fact-check '%s'",
+    id: "getcontext",
+    title: "Get Context: %s",
     contexts: ["selection"],
   },
   onCreated,
 );
 
 // Add sub-options for each fact-checking source
-for (let i = 0; i < factcheckers.length; i++) {
+for (let i = 0; i < searchengines.length; i++) {
   browser.contextMenus.create(
     {
-      id: factcheckers[i].id,
-      parentId: "contextify",
-      title: factcheckers[i].title,
+      id: searchengines[i].id,
+      parentId: "getcontext",
+      title: searchengines[i].title,
       contexts: ["selection"],
       icons: {
-        "32": factcheckers[i].logo
+        "32": searchengines[i].logo
       }
     }
   )
 }
 
-
 // Listen for click events
 browser.contextMenus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
-    case factcheckers[0].id:
-      browser.tabs.create({ url: factcheckers[0].url + info.selectionText });
+    case searchengines[0].id:
+      browser.tabs.create({ url: searchengines[0].url + processText(info.selectionText) });
       break;
-    case factcheckers[1].id:
-      browser.tabs.create({ url: factcheckers[1].url + info.selectionText });
+    case searchengines[1].id:
+      browser.tabs.create({ url: searchengines[1].url + processText(info.selectionText) });
       break;
-    case factcheckers[2].id:
-      browser.tabs.create({ url: factcheckers[2].url + info.selectionText });
+    case searchengines[2].id:
+      browser.tabs.create({ url: searchengines[2].url + processText(info.selectionText) });
       break;
-    case factcheckers[3].id:
-      browser.tabs.create({ url: factcheckers[3].url + info.selectionText });
+    case searchengines[3].id:
+      browser.tabs.create({ url: searchengines[3].url + processText(info.selectionText) });
       break;
   }
 });
-
-// Listen for messages from the background script
-
-// Open in new tab
-
-
 
 
 function onCreated() {  if (browser.runtime.lastError) {
@@ -64,4 +56,9 @@ function onCreated() {  if (browser.runtime.lastError) {
   } else {
     console.log("Context menu item created successfully.");
   }
+}
+
+function processText(text) {
+  // Strip punctuation
+  return text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\'\"]/g, "");
 }
